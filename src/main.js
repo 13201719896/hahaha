96143cc585671422ca0c6a17c65c56c587609b11
yangliguo <1547448357@qq.com>
2019-12-21 09:53:56 +0800

    axios配置

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import router from './router'
 import './styles/index.less'
 import ElementUI from 'element-ui' // 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import axios from 'axios'
 Vue.config.productionTip = false
+Vue.proyotype.$axios = axios
 Vue.use(ElementUI)
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 new Vue({
   router,
   render: h => h(App)
